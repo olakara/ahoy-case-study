cbf82e0f63ad22a70d3271e8148442ed7d58cce3
Abdel Raoof <olakara@gmail.com>
2022-06-11 11:02:00 +0400

    chore: Setting up DbContext and project dependencies

---
+++ src/Application/ConfigurationServices.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ConfigurationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());            
+
+            return services;
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {       
+
+        public ApplicationDbContext(IMediator mediator)
+        {
+        
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
